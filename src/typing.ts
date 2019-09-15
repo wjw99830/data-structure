44d52d91608e6b6e7ef13c1cd2e5643bfba2ad42
wjw <942404649@qq.com>
2019-09-15 12:13:03 +0800

    init

---
+++ src/typing.ts
@@ -0,0 +1,4 @@
+export interface ITreeNode {
+  children: ITreeNode[];
+  parent?: ITreeNode;
+}
---
+++ src/util.ts
@@ -0,0 +1,14 @@
+import { ITreeNode } from "./typing";
+
+export namespace Tree {
+  function printTree(tree: ITreeNode) {
+    
+  }
+  function getDepth(tree: ITreeNode) {
+    let depth = 0;
+    let current: ITreeNode | void = undefined;
+    while (current) {
+      
+    }
+  }
+}
