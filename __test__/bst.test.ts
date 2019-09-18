176958c470045912746901b8250fd23c8b6ecb8f
wjw <942404649@qq.com>
2019-09-18 22:46:31 +0800

    update: bst

--- __test__/bst.test.ts
+++ __test__/bst.test.ts
@@ -1,8 +1,44 @@
 import { BinarySearchTree } from "../src/bst";
 
 describe('traverse', () => {
-  it('should call fn for every node', () => {
+  it('should call handler for every node.', () => {
     const tree = new BinarySearchTree<number>();
     tree.insert(3).insert(5).insert(1).insert(2);
+    const arr: number[] = [];
+    tree.traverse(arr.push.bind(arr));
+    expect(arr).toEqual([1, 2, 3, 5]);
+  });
+});
+describe('asRoot', () => {
+  it('should return a subtree whose root value is same as passed.', () => {
+    const tree = new BinarySearchTree<number>();
+    tree.insert(3).insert(5).insert(1).insert(2);
+    const subTree = tree.asRoot(1);
+    // @ts-ignore
+    expect(subTree.root.value).toBe(1);
+    // @ts-ignore
+    expect(subTree.root.right.value).toBe(2);
+    // @ts-ignore
+    expect(subTree.root.left).toBe(undefined);
+  });
+});
+describe('contains', () => {
+  it('should return if the specified value exists.', () => {
+    const tree = new BinarySearchTree<number>();
+    tree.insert(3).insert(5).insert(1).insert(2);
+    expect(tree.contains(3)).toBe(true);
+    expect(tree.contains(1)).toBe(true);
+    expect(tree.contains(5)).toBe(true);
+    expect(tree.contains(2)).toBe(true);
+    expect(tree.contains(4)).toBe(false);
+  });
+});
+describe('remove', () => {
+  it('should remove a existing value.', () => {
+    const tree = new BinarySearchTree<number>();
+    tree.insert(3).insert(5).insert(1).insert(2);
+    expect(tree.contains(3)).toBe(true);
+    tree.remove(3);
+    expect(tree.contains(3)).toBe(false);
   });
 });
--- src/bst.ts
+++ src/bst.ts
@@ -1,8 +1,17 @@
 export class BinarySearchTree<T> {
   private root?: BinarySearchTreeNode<T>;
+  private equal: Equal<T> = (a, b) => a === b;
   constructor(
-    private lessThan: LessThan<T> = (a, b) => a < b
+    private lessThan: LessThan<T> = (a, b) => a < b,
   ) {}
+  asRoot(value: T, equalFn: Equal<T> = this.equal): BinarySearchTree<T> {
+    const subTree = new BinarySearchTree<T>();
+    subTree.root = this.root && this.root.get(value, equalFn);
+    return subTree;
+  }
+  contains(value: T, equalFn: Equal<T> = this.equal) {
+    return !!this.root && !!this.root.get(value, equalFn);
+  }
   insert(value: T) {
     if (this.root) {
       this.root.insert(value);
@@ -12,7 +21,16 @@ export class BinarySearchTree<T> {
     return this;
   }
   traverse(fn: TraverseHandler<T>) {
-    
+    if (this.root) {
+      this.root.traverse(fn);
+    }
+    return this;
+  }
+  remove(value: T) {
+    if (this.root) {
+      this.root.remove(value);
+    }
+    return this;
   }
 }
 class BinarySearchTreeNode<T> {
@@ -22,8 +40,9 @@ class BinarySearchTreeNode<T> {
     public value: T,
     private lessThan: LessThan<T>
   ) {}
+  // O(log(n))
   insert(value: T) {
-    if (this.lessThan(this.value, value)) {
+    if (this.lessThan(value, this.value)) {
       if (this.left) {
         this.left.insert(value);
       } else {
@@ -37,6 +56,46 @@ class BinarySearchTreeNode<T> {
       }
     }
   }
+  // O(log(n))
+  remove(value: T) {
+    if (this.value === value) {
+      let right = this.right;
+      while (right) {
+        this.value = right.value
+        if (right.right) {
+          right.value = right.right.value;
+        } else {
+          this.right = undefined;
+        }
+        right = right.right;
+      }
+    } else if (this.lessThan(value, this.value) && this.left) {
+      this.left.remove(value);
+    } else if (this.lessThan(this.value, value) && this.right) {
+      this.right.remove(value);
+    }
+  }
+  // O(n)
+  traverse(fn: TraverseHandler<T>) {
+    if (this.left) {
+      this.left.traverse(fn);
+    }
+    fn(this.value);
+    if (this.right) {
+      this.right.traverse(fn);
+    }
+  }
+  // O(log(n))
+  get(value: T, equalFn?: Equal<T>): BinarySearchTreeNode<T> | undefined {
+    if (equalFn ? equalFn(this.value, value) : this.value === value) {
+      return this;
+    } else if (this.lessThan(this.value, value) && this.right) {
+      return this.right.get(value);
+    } else if (this.lessThan(value, this.value) && this.left) {
+      return this.left.get(value);
+    }
+  }
 }
 type TraverseHandler<T> = (value: T) => any;
 type LessThan<T> = (a: T, b: T) => boolean;
+type Equal<T> = LessThan<T>;
